2cc529a7c500d984287edf4f87814a4cac73cc71
Santiago Gonzalez <santiagogonzaric1015@gmail.com>
2023-05-30 19:40:30 -0500

    Solucion ejercicio 2

---
+++ Ejercicio_2/index.js
@@ -0,0 +1,11 @@
+
+let numSelect = prompt("indicame un numero");
+
+numSelect=parseInt(numSelect);
+
+if (numSelect % 2 === 0 ) {
+    alert(numSelect + " es divisible entre 2");
+
+}else{
+    alert(numSelect + " no es divisible entre 2")
+}
