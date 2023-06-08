886eff5c653677245965a7be134f48e200fb1aab
Santiago Gonzalez <santiagogonzaric1015@gmail.com>
2023-06-07 22:27:40 -0500

    fix: Ajuste a la solucion ejercicio 6

--- Ejercicio_6/index.js
+++ Ejercicio_6/index.js
@@ -2,15 +2,14 @@ let num1 = parseInt(prompt("ingresa el primer valor"));
 let num2 = parseInt(prompt("ingresa el segundo valor"));
 let num3 = parseInt(prompt("ingresa el tercer valor"));
 
-if(num1 == num2 || num1 == num3 || num2 == num3){
-    console.log("Hay al menos dos números iguales.");
-if (num1 > num2 && num1 > num3) {   
-    console.log("El primer número (" + num1 + ") es el mayor.");
+if(num1 == num2 && num1 == num3 && num2 == num3){
+    alert("todos son iguales");
+} else if(num1 == num2 || num1 == num3 || num2 == num3){
+    alert("Hay al menos dos números iguales.");
+} else if(num1 > num2 && num1 > num3) {   
+    alert("El primer número (" + num1 + ") es el mayor.");
     } else if (num2 > num1 && num2 > num3) {
-        console.log("El segundo número (" + num2 + ") es el mayor.");
+        alert("El segundo número (" + num2 + ") es el mayor.");
     } else if (num3 > num1 && num3 > num2) {
-        console.log("El tercer número (" + num3 + ") es el mayor.");
-    } else{
-        console.log("todos son iguales");
+        alert("El tercer número (" + num3 + ") es el mayor.");
     }
-}   
